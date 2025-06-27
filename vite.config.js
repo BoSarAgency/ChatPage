d7df776ea8a6655be3a76e7658b139bb69892bac
Nick <wezawe@gmail.com>
2025-06-27 13:42:33 +0200

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // Allow all hosts
+    allowedHosts: "all", // Allow all hostnames (required for Replit)
     headers: {
       "X-Frame-Options": "ALLOWALL",
       "Content-Security-Policy": "frame-ancestors *",
