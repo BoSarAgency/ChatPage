844c97ecea5bbe5e26a797fe819e3a3b830f7ce6
Nick <wezawe@gmail.com>
2025-06-27 13:40:02 +0200

    Replit config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Allow all hosts
     headers: {
       "X-Frame-Options": "ALLOWALL",
       "Content-Security-Policy": "frame-ancestors *",
